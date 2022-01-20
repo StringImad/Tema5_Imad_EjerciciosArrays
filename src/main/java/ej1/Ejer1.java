47df53c6da715f1fa097f0c799afc5693b038bcc
stringimad <imadelfahssi@gmail.com>
2022-01-20 12:32:07 +0100

    ejer1 terminado

--- src/main/java/ej1/Ejer1.java
+++ src/main/java/ej1/Ejer1.java
@@ -9,5 +9,19 @@ package ej1;
  * @author imad
  */
 public class Ejer1 {
-    
+
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5, 6};
+        int suma = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+        for (int i = 0; i < numeros.length; i++) {
+            suma += numeros[i];
+        }
+        System.out.println("--------------suma-------------");
+
+        System.out.println(suma);
+    }
 }
