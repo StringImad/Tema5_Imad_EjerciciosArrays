5551bcdfc4017088c5f65fa07e26111becbb527c
stringimad <imadelfahssi@gmail.com>
2022-01-20 12:46:05 +0100

    Subida casi terminada de ejercicio 2 desde clase

--- src/main/java/ej2/Ejer2.java
+++ src/main/java/ej2/Ejer2.java
@@ -4,10 +4,46 @@
  */
 package ej2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author imad
  */
 public class Ejer2 {
-    
+
+    public static void main(String[] args) {
+        //Declaracion de variables
+        int numeroDePersonas;
+        int alturaPersona;
+        double media = 0.0;
+
+        int[] alturasLeidas;
+        //Declaracion de objetos
+        Scanner teclado = new Scanner(System.in);
+        do {
+            System.out.println("Introduce el numero de personas");
+            numeroDePersonas = teclado.nextInt();
+        } while (numeroDePersonas <= 0);
+        alturasLeidas = new int[numeroDePersonas];
+
+        for (int i = 0; i < alturasLeidas.length; i++) {
+            System.out.println("Introduce la altura en cm de la persona en la posicion: " + i);
+            alturaPersona = teclado.nextInt();
+            Math.abs(alturaPersona);
+            alturasLeidas[i] = alturaPersona;
+            media = media + alturasLeidas[i];
+
+        }
+        media = media / alturasLeidas.length;
+        
+        System.out.println("Media de altura es: "+media);
+        for (int alturasLeida : alturasLeidas) {
+                    if(alturasLeida<media){
+                        
+                    }
+
+        }
+
+    }
 }
